e71b8aa120b4af6dc059d5bafda31c3b1feaa2b5
Lars Kuijer <146839163+larsk1201@users.noreply.github.com>
2025-03-11 13:09:31 +0100

    Added workflows

---
+++ Jabberpoint-main/src/test/TestPresentation.java
@@ -0,0 +1,10 @@
+import org.junit.Test;
+import static org.junit.Assert.assertEquals;
+
+public class TestPresentation {
+
+    @Test
+    public void testAddition() {
+        assertEquals(2 + 2, 4);
+    }
+}
